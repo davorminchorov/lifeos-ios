aa4f099c07934dd3415c47358ee1539d05df47a6
Davor Minchorov <davorminchorov@gmail.com>
2024-09-22 03:44:42 +0200

    Initial Commit

---
+++ LifeiOSTests/LifeiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifeiOSTests.swift
+//  LifeiOSTests
+//
+//  Created by Davor Minchorov on 22.9.24.
+//
+
+import Testing
+@testable import LifeiOS
+
+struct LifeiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ LifeiOSUITests/LifeiOSUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LifeiOSUITestsLaunchTests.swift
+//  LifeiOSUITests
+//
+//  Created by Davor Minchorov on 22.9.24.
+//
+
+import XCTest
+
+final class LifeiOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
